66bafc02ad20f4365e3d4e7378adcccf21fefabd
Franlop24 <franlopbri@gmail.com>
2025-02-12 15:58:09 -0600

    08 Implements Modal Component

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "../componentes/Header";
+import Modal from "../componentes/Modal";
 
 export default function Layout() {
   return (
     <>
     <Header />
-    <Outlet />
+    <main className="container mx-auto py-16">
+      <Outlet />
+    </main>
+    <Modal />
     </>
   )
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,9 @@
 import { z } from "zod";
-import { CategoriesAPIResponseSchema, DrinkSchema, RecipesAPIResponseSchema, SearchFiltersSchema } from "../utils/recipes-schema";
+import { CategoriesAPIResponseSchema, DrinkSchema, RecipeAPIResponseSchema, RecipesAPIResponseSchema, SearchFiltersSchema } from "../utils/recipes-schema";
 
 
 export type Categories = z.infer<typeof CategoriesAPIResponseSchema>
 export type SearchFilters = z.infer<typeof SearchFiltersSchema>
 export type Recipes = z.infer<typeof RecipesAPIResponseSchema>
 export type Drink = z.infer<typeof DrinkSchema>
+export type Recipe = z.infer<typeof RecipeAPIResponseSchema>
