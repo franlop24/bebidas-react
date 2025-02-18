28d455c08b1fdbe615847c5c5f76c9e80da268c1
Franlop24 <franlopbri@gmail.com>
2025-02-18 11:10:03 -0600

    11 Implements favoritesSlice and FavoritesPage

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,8 +1,10 @@
 import { create } from "zustand";
 import { createRecipeSlice, RecipesSliceType } from "./recipeSlice";
 import { devtools } from "zustand/middleware";
+import { createFavoritesSlice, FavoritesSliceType } from "./favoritesSlice";
 
 
-export const useAppStore = create<RecipesSliceType>()(devtools((...a) => ({
-    ...createRecipeSlice(...a)
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(devtools((...a) => ({
+    ...createRecipeSlice(...a),
+    ...createFavoritesSlice(...a)
 })))
